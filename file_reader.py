d5eba6f1c4efc8f0b11e6160447e873c632f5899
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-26 01:41:43 +0100

    added code for reading an entire file, reading line by line & making a list from a file

--- file_reader.py
+++ file_reader.py
@@ -1,3 +1,25 @@
+# Reading an entire file
 with open("pi_digits.txt") as file_object:
     contents = file_object.read()
-    print(contents)
+print(contents)
+# print(contents.rstrip())
+print()
+
+
+# Reading line by line
+filename = "pi_digits.txt"
+
+with open(filename) as file_object2:
+    for line in file_object2:
+        # print(line)
+        print(line.rstrip())
+print()
+
+
+# Making a list from a file
+with open(filename) as file_object3:
+    lines = file_object3.readlines()
+print(lines)
+
+for line in lines:
+    print(line.rstrip())
