687b9b4269682bcc9976a7e236998d881089fe18
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-27 08:02:25 +0100

    wrapped the code in a while loop and making the calculator able to handle errors without crashing

--- exceptions.py
+++ exceptions.py
@@ -3,11 +3,21 @@ that occurs when a user inputs a string"""
 
 print("Give me ant 2 numbers and i will add them\n")
 
-try:
-    first_number = int(input("first number: "))
-    second_number = int(input("second number: "))
-except ValueError:
-    print("you can only input a number")
-else:
-    add = first_number + second_number
-    print(add)
+while True:
+    first_number = input("first number: ").lower()
+    # condition to be met for breaking the loop
+    if first_number == "off":
+        break
+
+    second_number = input("second number: ").lower()
+    if second_number == "off":
+        break
+
+    # add a try except block to catch any ValueError that may occur
+    # when user tries to add alphabets with numbers
+    try:
+        add = int(first_number) + int(second_number)
+    except ValueError:
+        print("you can only add numbers.")
+    else:
+        print(f"This is the sum: {add}\n")
