a4db1f206a13c21dd509fcc342749f1ee22ef640
Chukwukagideon <Chukwukagideon5@gmail.com>
2024-12-27 08:22:19 +0100

    modified the except block to fail silently.

--- pets_file_reader.py
+++ pets_file_reader.py
@@ -1,11 +1,14 @@
 # filename = "cats.txt"
 
 def file_reader(filename):
+    """A function to get the file, read and print the contents to the screen"""
     try:
+        # open the file as read only
         with open(filename) as file_object:
             pet_names = file_object.read()
     except FileNotFoundError:
-        print(f"Requested {filename} not found!")
+        # print(f"Requested {filename} not found!")
+        pass # make the code fail silently. i.e. don't give any report to the user, but the code should keep running.
     else:
         print(pet_names + "\n")
 
