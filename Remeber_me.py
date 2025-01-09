7360c0e385bd1e632174f2b0590cd22b03249341
Chukwukagideon <Chukwukagideon5@gmail.com>
2025-01-09 12:41:50 +0100

    LEARNT REFACTORING. REFACTORED THE CODE INTO FUNCTIONS

--- Remeber_me.py
+++ Remeber_me.py
@@ -14,6 +14,7 @@ with open(filename, "w") as file_object:
     print("We will remember your name next time!")"""
 
 
+"""
 # using the try except block to attempt to recover a username and if the username is not saved
 # it prompts the user to input their name.
 
@@ -31,4 +32,53 @@ except FileNotFoundError:
     with open(filename, "w") as file_object:
         json.dump(username, file_object)
 else:
-    print(f"Welcome back {username}")
+    print(f"Welcome back {username}")"""
+
+
+
+# --------------------REFACTORING-------------------
+# REFACTORING THE ABOVE CODE, MOVING THE BULK OF THE LOGIC INTO FUNCTIONS
+
+import json
+
+
+def get_stored_username():
+    """A function that gets the stored username from the json file"""
+    filename = "username.json"
+    try:
+        with open(filename) as file_object:
+            username = json.load(file_object)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """A function that prompts for a new username"""
+
+    filename = "username.json"
+    username = input("What is your name? ")
+
+    with open(filename, "w") as file_object:
+        json.dump(username, file_object)
+
+    return username
+
+
+def greet_user():
+    """A function to greet a user by name"""
+    username = get_stored_username()
+
+    if username:
+        print(f"Welcome back {username}")
+    else:
+        username = get_new_username()
+        print(f"We will remember your name next time, {username}")
+
+
+greet_user()
+
+
+
+
